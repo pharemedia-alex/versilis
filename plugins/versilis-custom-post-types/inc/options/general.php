59e0ea3a02044ad4415f3715fe5ab14825ed6cfa
alex <alex@pharemedia.com>
2021-11-05 14:28:56 -0400

    update and new plugins

--- plugins/versilis-custom-post-types/inc/options/general.php
+++ plugins/versilis-custom-post-types/inc/options/general.php
@@ -9,14 +9,27 @@
 //Add option page 
 if( function_exists('acf_add_options_page') ) {
 	
-	acf_add_options_page(array(
-		'page_title' 	=> __( 'Theme options', 'banq-ntni-cc' ),
-		'menu_title'	=> 'Theme options',
-		'menu_slug' 	=> 'theme-general-settings',
-		'capability'	=> 'edit_posts',
-		'redirect'		=> false
+	// Add parent.
+	$parent = acf_add_options_page(array(
+		'page_title'  => __('Theme options'),
+		'menu_title'  => __('Theme options'),
+		'redirect'    => true,
+));
+
+	// Add sub page.
+	$child = acf_add_options_sub_page(array(
+			'page_title'  => __('General settings'),
+			'menu_title'  => __('General settings'),
+			'parent_slug' => $parent['menu_slug'],
 	));
-	
+
+	// Add sub page.
+	$child = acf_add_options_sub_page(array(
+		'page_title'  => __('Mega menu'),
+		'menu_title'  => __('Mega menu'),
+		'parent_slug' => $parent['menu_slug'],
+));
+
 	//create sub menu option page
 	/*acf_add_options_sub_page(array(
 		'page_title' 	=> 'Theme Header Settings',
